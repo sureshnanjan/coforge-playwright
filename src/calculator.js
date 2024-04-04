f395cf04d5323ec3f02ff49a9cfe24bc8b5d5440
sureshnanjan <sureshnanjan@outlook.com>
2024-04-05 00:09:36 +0530

    Refactored file names

--- src/calculator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.add = void 0;
-function add(a, b) {
-    return (a + b).toString(); // TODO : FIX ERROR so that it returns a number
-}
-exports.add = add;
