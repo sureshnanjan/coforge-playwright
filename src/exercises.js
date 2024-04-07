557b95ecccc19e4cd48dd420fab619cc30b27d0b
Imran-km <kmimran143@gmail.com>
2024-04-07 10:39:36 +0530

    My first code push

---
+++ src/exercises.js
@@ -0,0 +1,63 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.exercise5 = exports.exercise4 = exports.exercise3 = exports.exercise2 = exports.exercise1 = exports.add = void 0;
+/**
+ * This function is a simple arithmetic adder
+ * @param a  First number to add
+ * @param b  Second number to add
+ * @returns The sum of First and Second numbers
+ */
+function add(a, b) {
+    return (a + b).toString();
+    // TODO : FIX ERROR so that it returns a number
+    // NOT STRING
+}
+exports.add = add;
+/**
+* This function is expected to return 100
+* @returns  - 100 is returned always
+*/
+function exercise1() {
+    return 0; //FIX THE CODE TO OBEY SPEC
+}
+exports.exercise1 = exercise1;
+/**
+ * This method will apply the prefix and suffix to the word
+ * HELLO and send back the results
+ * @param prefix - Appears before HELLO
+ * @param suffix  - Appears after HELLO
+ * @returns
+ */
+function exercise2(prefix, suffix) {
+    // Implement code as per spec
+    return "";
+}
+exports.exercise2 = exercise2;
+/**
+ * This function returns the length of the word passed as the argument
+ * @param word - The input word for which length is needed
+ * @returns - The length of the word
+ */
+function exercise3(word) {
+    return 0;
+}
+exports.exercise3 = exercise3;
+/**
+ * This method is supposed to return a value as per the
+ * method signature declaration
+ * @returns A value obeying the method contract
+ */
+function exercise4() {
+    return 10;
+    // FIX this so that it obeys the signature
+}
+exports.exercise4 = exercise4;
+/**
+ * This function returns the reversed version of the input
+ * @param input - The actual string passed to function
+ * @returns  - The reversed string
+ */
+function exercise5(input) {
+    return "";
+}
+exports.exercise5 = exercise5;
