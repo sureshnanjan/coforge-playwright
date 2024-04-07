1c9388ea70e31a80ac47cfd2834a67dcf61465a0
Imran-km <kmimran143@gmail.com>
2024-04-07 16:48:43 +0530

    Added new PW Code

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,9 +2,14 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/');
-
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('The Internet');
+  const totalLinks = (await page.$$("#content>ul>li>a")).length
+  await expect(totalLinks).toEqual(44);
+
+
 });
+
+
 
 
