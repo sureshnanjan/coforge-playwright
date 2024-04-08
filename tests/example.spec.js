ecceaef393523e59f1de58a514afa402144321e1
ShwethaH <shwetha.1.h@coforge.com>
2024-04-08 16:11:10 +0530

    Changes applied

---
+++ tests/example.spec.js
@@ -0,0 +1,31 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const test_1 = require("@playwright/test");
+(0, test_1.test)('has title', (_a) => __awaiter(void 0, [_a], void 0, function* ({ page }) {
+    yield page.goto('https://the-internet.herokuapp.com/');
+    // Expect a title "to contain" a substring.
+    yield (0, test_1.expect)(page).toHaveTitle('The Internet');
+    (0, test_1.test)('TitleandsubTitle', (_b) => __awaiter(void 0, [_b], void 0, function* ({ page }) {
+        yield page.goto('https://the-internet.herokuapp.com/');
+        yield (0, test_1.expect)(page.getByRole('heading', { name: 'Welcome to the-internet' })).toContainText("Welcome to the-internet");
+        yield (0, test_1.expect)(page.getByRole('heading', { name: 'Available Examples' })).toContainText("Available Examples");
+    }));
+    (0, test_1.test)('Home page heading and subheading', (_c) => __awaiter(void 0, [_c], void 0, function* ({ page }) {
+        yield page.goto('https://the-internet.herokuapp.com/');
+        //const tit=await page.getByRole('heading', { name: 'Welcome to the-internet' });
+        yield (0, test_1.expect)(page.getByRole('heading', { name: 'Welcome to the-internet' })).toHaveText('Welcome to the-internet12');
+        //console.assert(page.getByRole('heading', { name: 'Welcome to the-internet' }),'Welcome to the-internet6');
+        //const tit1=await page.getByRole('heading', { name: 'Available Examples6' });
+        yield (0, test_1.expect)(page.getByRole('heading', { name: 'Available Examples' })).toHaveText('Available Examples');
+        //console.assert(tit1,"true");
+    }));
+}));
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,6 +5,25 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('The Internet');
+
+  test('TitleandsubTitle', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+    await expect(page.getByRole('heading', { name: 'Welcome to the-internet' })).toContainText("Welcome to the-internet")
+    await expect(page.getByRole('heading', { name: 'Available Examples' })).toContainText("Available Examples")
+   
+  })
+
+test('Home page heading and subheading', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/');
+  //const tit=await page.getByRole('heading', { name: 'Welcome to the-internet' });
+  await expect(page.getByRole('heading', { name: 'Welcome to the-internet' })).toHaveText('Welcome to the-internet12');
+  //console.assert(page.getByRole('heading', { name: 'Welcome to the-internet' }),'Welcome to the-internet6');
+ 
+  //const tit1=await page.getByRole('heading', { name: 'Available Examples6' });
+  await expect(page.getByRole('heading', { name: 'Available Examples' })).toHaveText('Available Examples');
+  //console.assert(tit1,"true");
+});
+
 });
 
 
