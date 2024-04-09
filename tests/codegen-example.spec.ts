761fd62c3cf47a68704eb3fee9ab7cdca08d8d48
shikander <shikander.raja@gmail.com>
2024-04-09 22:19:44 +0530

    Added Codegen test case

--- tests/codegen-example.spec.ts
+++ tests/codegen-example.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/');
-  await page.getByRole('heading', { name: 'Welcome to the-internet' }).click();
-  await page.getByRole('heading', { name: 'Available Examples' }).click();
+  await expect(page.getByRole('heading', { name: 'Welcome to the-internet' })).toHaveText('Welcome to the-internet');
+  await expect(page.getByRole('heading', { name: 'Available Examples' })).toHaveText('Available Examples');
 });
