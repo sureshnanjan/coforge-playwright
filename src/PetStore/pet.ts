375c591fa478910af05664a0b6913a6683159fd2
“madhukarreddy00” <madhukarreddy00@gmail.com>
2024-04-10 00:41:36 +0530

    adding inheritance spec

---
+++ src/PetStore/pet.ts
@@ -0,0 +1,47 @@
+/**
+* Category uses to define a common values from Pet
+*
+*/
+export class Category {
+    id: number;
+    name: string;
+ 
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+    /**
+     *
+     * @returns category Id and Name of the pet
+     */
+    petCategory(): string {
+        return `Pet Category ID: ${this.id}, name: ${this.name}`;
+    }
+}
+ 
+/**
+* Pet Details class uses to store petdetails along with common members from Category class
+*
+*/
+export class PetDetails extends Category {
+    petId: number;
+    name: string;
+    photoUrls: string[];
+    tags: any[];  
+    status: string;
+ 
+    constructor(id: number, category: Category, name: string, photoUrls: string[], tags: any[], status: string) {
+        super(category.id, category.name);
+        this.petId = id;
+        this.name = name;
+        this.photoUrls = photoUrls;
+        this.tags = tags;
+        this.status = status;
+    }
+ 
+    getPetDetails(): string {
+        return `Pet ID: ${this.petId}, Name: ${this.name}}`;
+    }
+}
+ 
+ 
