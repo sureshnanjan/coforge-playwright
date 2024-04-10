fe49077adcdb84ff767161d6ab379765e17118d7
amojes93 <166160055+amojes93@users.noreply.github.com>
2024-04-10 16:49:10 +0530

    latest changes 10054

---
+++ src/PetStore/checkpromise.ts
@@ -0,0 +1,24 @@
+/**
+ * Examples for Promise with resolve and reject 
+ */
+
+const _checkPromise = new Promise(function(pass, fail){
+    let a = 'venk5522';
+    let b= 'venk1234';
+    if(a === b){
+        pass(`Both variables values are matching ${a} == ${b}`)
+    }else{
+        fail(`Both variables values are not matching ${a} !== ${b}`)
+    }
+    })
+
+    _checkPromise
+    .then(function (pass) {
+        
+         console.log(pass);
+    }, 
+    function (fail) {
+
+        console.log(fail);
+
+    });
---
+++ tests/day2assignment.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('The Internet');
+});
+
+test('Home Page Has 44 Examples', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/');
+  await expect(page.locator('li')).toHaveCount(46);
+});
+
+test('Heading and SubheadIng Matches', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/');
+  await page.getByRole('heading', { name: 'Welcome to the-internet' }).click({
+    button: 'right'
+  });
+  await page.getByRole('heading', { name: 'Welcome to the-internet' }).click({
+    button: 'right'
+  });
+});
