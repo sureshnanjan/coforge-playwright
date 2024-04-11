df1f19a8811c63c162d13298c84ca41ac78eb46b
sureshnanjan <sureshnanjan@outlook.com>
2024-04-11 23:40:35 +0530

    Cleanup Folder structure

---
+++ src/features/steps/heroku_homepage_steps.ts
@@ -0,0 +1,13 @@
+import {Given,When,Then} from "@cucumber/cucumber"
+Given('I visit heroku app', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+When('I check title', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+Then('I have contents', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
