566cf21de0f03a791e629b3da460bab27d9a6a88
sureshnanjan <sureshnanjan@outlook.com>
2024-04-14 16:01:09 +0530

    Page Framework

---
+++ src/pages/abtesting_page.ts
@@ -0,0 +1,43 @@
+import { Page } from "@playwright/test";
+
+/**
+ * 
+ */
+export class ABTestingPage{
+    readonly page:Page
+    /**
+     * 
+     * @returns 
+     */
+    getHeading():Promise<null|string>{
+        // return the heading
+        return "";
+        // #content > div > h3 - CSS Selector
+        // //*[@id="content"]/div/h3 = XPath
+        // ARIA Roles 
+        //this.page.locator("")
+        return this.page.getByRole("heading").textContent();
+
+    }
+
+    /**
+     * 
+     * @returns 
+     */
+
+    getDescriptionText():string{
+
+        // code to return the text of paragraph
+        return "";
+    }
+
+    // This is my help
+    /**
+     * This is a mechanism to disable AB Testing 
+     */
+    disableABTesting(){
+        // Do athe automatic setting of cookie
+        this.page.context().addCookies([{"name":'optimizelyOptOut',"value": 'true'}])
+    }
+
+}
