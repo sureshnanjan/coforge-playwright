f3b5d510a3ac93bd4b562efbc55e075c85615a60
Imran-km <kmimran143@gmail.com>
2024-04-14 14:31:13 +0530

    Added BasicAuth and StatusCode Pages

---
+++ pages/heroku_basicauth_page.ts
@@ -0,0 +1,24 @@
+import { Page } from "@playwright/test"
+
+
+export class BasicAuthPage{
+
+     getTitle(){
+        return "";
+    }
+
+    clickBasicAuthButton(){
+
+    }
+
+    enterAuthUserName(username:string){
+
+    }
+    enterAuthPassword(password:string){
+        
+    }
+
+    validateStatus: any{
+        
+    }
+}
---
+++ pages/heroku_statuscodes_page.ts
@@ -0,0 +1,41 @@
+import { Page } from "playwright";
+
+export class StatusCodesPage{
+
+    clickOntatusCodesBtn(){
+    
+    }
+
+    verifyStatusCodesHeader(){
+
+    }
+
+    clickOnStatusCode200(){
+
+    }
+    verifyStatusCodeSubHeadingOf200(){
+
+    }
+    clickOnStatusCode301(){
+        
+    }
+    verifyStatusCodeSubHeadingOf301(){
+        
+    }
+    clickOnStatusCode404(){
+        
+    }
+    verifyStatusCodeSubHeadingOf404(){
+        
+    }
+    clickOnStatusCode500(){
+        
+    }
+    verifyStatusCodeSubHeadingOf500(){
+        
+    }
+
+
+
+
+}
